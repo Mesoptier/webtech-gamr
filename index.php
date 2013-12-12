8ae39101016191eadd8f5a4eba61c02c98cceace
tsegers <t.segers93@gmail.com>
2013-12-12 10:53:01 +0100

    Updated index and added a header

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML>
+<!--
+  ____    _    __  __ ____  
+ / ___|  / \  |  \/  |  _ \ 
+| |  _  / _ \ | |\/| | |_) |
+| |_| |/ ___ \| |  | |  _ < 
+ \____/_/   \_\_|  |_|_| \_\
+
+ Project:  Webtech GAMR 
+ Repo:     mesoptier/webtech-gamr
+ Date:     12-12-2013
+ Lang:     PHP
+
+ Last Edit:        12-12-2013
+ Version:          0.1
+ -->
+                            
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    
         <title>GAMR - Main Page</title>
-        
     </head>
+
     <body>
         <div id="Header">
             
         </div>
 
-        <div id="Content">
-            <?php
-                require_once("lorem.php");
-            ?>
+        <div id="Main">
+            require_once("lorem.php")            
         </div>
 
         <div id="Footer">
