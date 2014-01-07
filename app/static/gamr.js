a986b04031b172d4cbfc7eb18af35aa2562c0047
Koen Klaren <koen.klaren@gmail.com>
2014-01-07 13:30:16 +0100

    Search results

--- app/static/gamr.js
+++ app/static/gamr.js
@@ -1,16 +1,32 @@
 $(function(){
     $(".search-input").on("input", function(){
-        $.ajax({
-            url: "http://localhost/webtech-gamr/app/search-results",
-            success: function(data){
-                $(".results").empty();
-
-                for (var i = 0; i < data.length; i++){
-                    $(".results").append(
-                        $("<li>").text(data[i].title)
-                    );
-                }
-            }
-        })
+        requestSearchResults($(this).val());
     });
 });
+
+var searchWait = false;
+function requestSearchResults(search){
+    if (!searchWait){
+        searchWait = true;
+        setTimeout(function(){
+            $.ajax({
+                url: "http://localhost/webtech-gamr/app/search-results",
+                success: function(data){
+                    $(".results").empty();
+
+                    if (data.length > 0){
+                        for (var i = 0; i < data.length; i++){
+                            $(".results").append(
+                                "<li class=\"result-item\"><a href=\"" +
+                                data[i].url + "\">" + data[i].title + "</a></li>");
+                        }
+                    }
+                },
+
+                complete: function(){
+                    searchWait = false;
+                }
+            });
+        }, 500);
+    }
+}
